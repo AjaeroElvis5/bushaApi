6307d43e3fe99f59ecc0f4e9aea677d838727a6c
AjaeroElvis5 <AjaeroElvis5@gmail.com>
2025-05-15 15:12:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "ajaeroElvis5@gmail.com";
-const pass = "qrgdnnovrfqeyutr";
+//const pass = "qrgdnnovrfqeyutr";
 // 15 MAY
 
 // Middleware
